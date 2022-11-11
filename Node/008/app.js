161e32b5c2dcdd2b490ba2471d45d514ba78ad7a
곤 <konveloper@gmail.com>
2022-11-12 00:53:46 +0900

    add: 11월 9일 학습 내용

---
+++ Node/008/app.js
@@ -0,0 +1,13 @@
+// 노드로 서버 만들기
+
+const http = require('http');
+const server = http.createServer(function(req, res){
+    console.log('server가 구동중입니다.');
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    res.write('hello world');
+    res.end();
+});
+
+server.listen(8080);
